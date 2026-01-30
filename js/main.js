4abeb6d2c799d929cdfe042ecddd4cd5eaaebc23
mckydot <ckyfirstcom@gmail.com>
2026-01-30 18:50:31 +0900

    안전상황 ui표시 버튼

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const danger = document.querySelector(".danger");
 const value = document.querySelector(".value");
 const inclinationValue = document.querySelector(".inclinationValue");
 const inclinationValue2 = document.querySelector(".inclinationValue2");
-
+const change2 = document.getElementById("change2");
 
 emergency_wrap.addEventListener("click", ()=>{
     alert("긴급상황 발생!");
@@ -21,3 +21,13 @@ change.addEventListener("click", ()=>{
     inclinationValue2.classList.remove("hidden");
     inclinationValue.classList.add("hidden");
 });
+
+change2.addEventListener("click", ()=>{
+    alertBtn.classList.add("hidden");
+    danger.classList.add("hidden");
+    safe.classList.remove("hidden");
+    value.classList.add("valueColor1");
+    value.classList.remove("valueColor2");
+    inclinationValue2.classList.add("hidden");
+    inclinationValue.classList.remove("hidden");
+})
