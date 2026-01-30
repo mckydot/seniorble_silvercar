58fe1c807282977c4444e520d1b299f481e0fe4c
mckydot <ckyfirstcom@gmail.com>
2026-01-30 18:36:48 +0900

    위험 ui로 변환하는 버튼 추가(개발용)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const emergency_wrap = document.getElementById("emergency_wrap");
+const change = document.getElementById("change");
+const alertBtn = document.querySelector(".alert");
+const safe = document.querySelector(".safe");
+const danger = document.querySelector(".danger");
+const value = document.querySelector(".value");
+const inclinationValue = document.querySelector(".inclinationValue");
+const inclinationValue2 = document.querySelector(".inclinationValue2");
+
 
 emergency_wrap.addEventListener("click", ()=>{
     alert("긴급상황 발생!");
+});
+
+change.addEventListener("click", ()=>{
+    alertBtn.classList.remove("hidden");
+    danger.classList.remove("hidden");
+    safe.classList.add("hidden");
+    value.classList.remove("valueColor1");
+    value.classList.add("valueColor2");
+    inclinationValue2.classList.remove("hidden");
+    inclinationValue.classList.add("hidden");
 });
