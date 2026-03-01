83907cfdc75dc65d3fad2c1c5747f7977d9e0609
mckydot <ckyfirstcom@gmail.com>
2026-03-01 14:51:14 +0900

    cors 수정

--- back/server.js
+++ back/server.js
@@ -88,7 +88,7 @@ const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://mckydot.github.io',
-    'https://mckydot.github.io/'
+    'https://mckydot.github.io'
 ];
 if (process.env.FRONTEND_URL && !allowedOrigins.includes(process.env.FRONTEND_URL)) {
     allowedOrigins.push(process.env.FRONTEND_URL);
